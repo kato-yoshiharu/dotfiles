c627cd3aaef396a3536f6ca7b68db96c5853b5be
kato-yoshiharu <yoshiharu2580@gmail.com>
2026-09-10 19:38:26 +0900

    weztermでcmd+enterが効くように設定

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,4 +11,13 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
 
+config.keys = {
+  -- CMD+Enter を ALT+Enter として送る。受け手（Claude Code）は meta+enter として解釈する。
+  {
+    key = "Enter",
+    mods = "CMD",
+    action = wezterm.action.SendKey({ key = "Enter", mods = "ALT" }),
+  },
+}
+
 return config
